1e58740f1ab333beded1fc36d13645d014ef47e3
Neemias Claudio <neemiasclaudio5@gmail.com>
2024-05-27 17:24:27 -0300

    concertando keys faltantes

--- src/app/components/Catalogo2/Catalogo2.tsx
+++ src/app/components/Catalogo2/Catalogo2.tsx
@@ -36,8 +36,8 @@ const Catalogo2 = () => {
         </div>
       ) : (
         <div className="Catalogo2Container">
-          {divsArray.map(() => (
-            <Catalogo2Loader />
+          {divsArray.map((item: any) => (
+            <Catalogo2Loader key={item} />
           ))}
         </div>
       )}
